1ec512a8049abd4238a4f3dc207a7245e4b2c6e9
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2022-10-02 01:29:50 -0300

    Adding a new component with test

---
+++ src/Sidebar.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Sidebar({ items }: { items: { name: string; href: string }[] }) {
+  return (
+    <div>
+      {items.map(item => (
+        <div key={item.href}>
+          <a role="navigation" href={item.href}>
+            {item.name}
+          </a>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Sidebar;
