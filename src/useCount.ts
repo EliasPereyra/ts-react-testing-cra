eb60365de07d3257d0960a979fdec6cb0089a64e
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-01-04 12:24:06 -0300

    Move each file to its correspondent folder

--- src/useCount.ts
+++
@@ -1,8 +0,0 @@
-import { useState, useCallback } from 'react'
-
-export function useCount() {
-  const [count, countSet] = useState(0)
-
-  const increment = useCallback(() => countSet((x) => x + 1), [])
-  return { count, increment }
-}
