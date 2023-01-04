7c629b25ac3dcecba82f00c18359fc0351437ac2
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-01-04 12:27:27 -0300

    Replace the getbyrole func to findbyrole

--- src/components/APIComponent/APIComponent.test.tsx
+++ src/components/APIComponent/APIComponent.test.tsx
@@ -1,7 +1,8 @@
+import { render, screen, waitFor } from "@testing-library/react";
 import { rest } from "msw";
 import { setupServer } from "msw/node";
+
 import APIComponent from "./APIComponent";
-import { render, screen, waitFor } from "@testing-library/react";
 
 const server = setupServer(
   rest.get("/api", (req, res, ctx) => {
@@ -16,7 +17,7 @@ afterAll(() => server.close());
 test("gets the data", async () => {
   render(<APIComponent />);
 
-  const result = await waitFor(() => screen.getByRole("contentinfo"));
+  const result = await waitFor(() => screen.findByRole("contentinfo"));
 
   expect(result).toHaveTextContent("My name is Elias");
 });
