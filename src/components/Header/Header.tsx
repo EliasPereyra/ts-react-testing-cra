db27b0cbcb14333f94996720a1711680648c6bfe
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-01-13 18:35:43 -0300

    Create a Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <section className="header">
+      <ul className="header-items">
+        <li>
+          <a>Home</a>
+        </li>
+        <li>
+          <a>Person</a>
+        </li>
+      </ul>
+    </section>
+  );
+}
