55f834617d61602ce7842aa69c7f41b565bd5ad9
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-02-17 23:52:16 -0300

    Add mock data for rendering the sidebar component correctly

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
 import { Header } from "./components/Header/Header";
 
 import "./App.css";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Counter from "./components/Counter/Counter";
 
 function App() {
+  const items = [
+    {
+      name: "First item",
+      href: "#",
+    },
+    {
+      name: "Second item",
+      href: "#",
+    },
+    {
+      name: "Third item",
+      href: "#",
+    },
+  ];
+
   return (
-    <div className="App">
+    <div className="app">
       <Header />
+      <div>
+        <Sidebar items={items} />
+      </div>
+      <Counter />
     </div>
   );
 }
